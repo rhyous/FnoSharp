df1d0e5fe1ae979a1747954efe7295e780f82012
rhyous <rhyous@yahoo.com>
2017-01-24 16:25:51 -0700

    Added OrganizationBuilder

---
+++ src/FnoSharp/Builder/OrganizationBuilder.cs
@@ -0,0 +1,23 @@
+﻿namespace FnoSharp.Builder
+{
+    public class OrganizationBuilder : BuilderBase<organizationDataType>
+    {
+        public string Id
+        {
+            get { return Object.name; }
+            set { Object.name = value; }
+        }
+        
+        public string displayName
+        {
+            get { return Object.displayName; }
+            set { Object.displayName = value; }
+        }
+
+        public string Description
+        {
+            get { return Object.description; }
+            set { Object.description = value; }
+        }
+    }
+}
